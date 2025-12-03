0f5d1fca6e4596b9c5b66769a588131424aff925
huentb <huentb98@gmail.com>
2025-12-03 20:50:33 +0700

    init

---
+++ src/main/java/feature/TestDemo.java
@@ -0,0 +1,16 @@
+package feature;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestDemo {
+    static WebDriver driver = null;
+
+    public static void main(String[] agrs) {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();;
+        driver.get("https://link.com");
+//        driver.quit();
+    }
+}
