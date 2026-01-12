caa8498dcc399d8ca5a528970dfc42842045e4ba
huentb <huentb98@gmail.com>
2026-01-12 21:30:42 +0700

    homeworkd14

--- src/test/java/feature/LoginMultiCaseTest.java
+++ src/test/java/feature/LoginMultiCaseTest.java
@@ -1,4 +1,97 @@
 package feature;
 
+import com.google.common.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.testng.*;
+import org.testng.annotations.*;
+
 public class LoginMultiCaseTest {
+    WebDriver driver;
+
+    @BeforeSuite
+    public void setUpSuite() {
+        System.out.println("===== Test Suite Started =====");
+    }
+
+    @BeforeMethod // mỗi test mở lại trang mới
+    public void setUp() {
+        System.out.println("===== Before Each Test Method =====");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @BeforeClass // chỉ mở trang web duy nhất 1 lần
+    public void setUpClass() {
+        System.out.println("===== Test Class Setup =====");
+    }
+
+    @Test
+    public void testLoginWithValidCredentials() {
+        driver.get("https://www.saucedemo.com/");
+
+        //kiem tra tieu de va url
+        Assert.assertEquals(driver.getTitle(), "Swag Labs",
+                "Title mismatch!");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/",
+                "URL mismatch!");
+
+        //dien thong tin dung
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        //xac nhan login thanh cong, vao trang inventory
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html",
+                "Home URL mismatch!"); //co the ktra them cac element khac
+    }
+
+    @Test
+    public void testLoginWithUsernameNonExistentUser() {
+        driver.get("https://www.saucedemo.com/");
+
+        //dien thong tin dung
+        driver.findElement(By.id("user-name")).sendKeys("standard_user1");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        //bao loi
+        WebElement errorMsg = driver.findElement(By.xpath("//h3[@data-test='error']"));
+        Assert.assertTrue(errorMsg.isDisplayed(),"Error message is display!");
+        Assert.assertTrue(errorMsg.getText().contains("Username and password do not match"),"Failed to log in with a non-existent user!");
+    }
+
+    @Test
+    public void testLoginWithLockedOutUser() {
+        driver.get("https://www.saucedemo.com/");
+
+        //dien thong tin dung
+        driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        //bao loi
+        WebElement errorMsg = driver.findElement(By.xpath("//h3[@data-test='error']"));
+        Assert.assertTrue(errorMsg.isDisplayed(),"Error message is display!");
+        Assert.assertTrue(errorMsg.getText().contains("Sorry, this user has been locked out"),"Fail to login with locked out user!");
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        System.out.println("===== Test Class Tear Down =====");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("===== After Each Test Method =====");
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @AfterSuite
+    public void tearDownSuite() {
+        System.out.println("===== Test Suite Complete =====");
+    }
+
 }
--- src/test/java/feature/homeworkd14/BookRequestDemo.java
+++ src/test/java/feature/homeworkd14/BookRequestDemo.java
@@ -1,4 +1,127 @@
 package feature.homeworkd14;
 
+import D13.homework.exercise1.RequestDemoPage;
+import common.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.support.ui.*;
+import org.testng.*;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+import java.util.*;
+
 public class BookRequestDemo {
+    WebDriver driver;
+
+    @BeforeSuite
+    public void setUpSuite() {
+        System.out.println("===== Test Suite Started =====");
+    }
+
+    @BeforeClass // chỉ mở trang web duy nhất 1 lần
+    public void setUpClass() {
+        System.out.println("===== Test Class Setup =====");
+    }
+
+    @BeforeMethod // mỗi test mở lại trang mới
+    public void setUp() {
+        System.out.println("===== Before Each Test Method =====");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void inputValidData() {
+        driver.get("https://saucelabs.com/request-demo");
+
+        //nhap data vao page oject trong main
+        String excelFilePath = "D:\\Automation Testing\\AutoTest\\AutoTestD1/dataInfo.xlsx";
+        String sheetName = "Sheet1";
+
+        //doc du lieu tu file excel
+        List<Map<String, String>> excelData = ExcelUtils.readExcelData(excelFilePath, sheetName);
+
+        RequestDemoPage demoPage = new RequestDemoPage(driver);
+
+
+        Map<String, String> data = excelData.get(0);
+
+        demoPage.open();
+
+        demoPage.fillForm(
+                data.get("Business Email"),
+                data.get("First Name"),
+                data.get("Last Name"),
+                data.get("Company"),
+                data.get("Phone Number"),
+                data.get("Country"),
+                data.get("Interest"),
+                data.get("Comments")
+        );
+        //xac nhan nhap dung data, khong có alert
+        List<WebElement> alerts = driver.findElements(By.xpath("//div[@role='alert']"));
+        Assert.assertTrue(alerts.isEmpty(), "No alert on the screen!");
+    }
+
+    @Test
+    public void interestDataIsMissing() throws InterruptedException {
+        driver.get("https://saucelabs.com/request-demo");
+
+        //nhap data vao page oject trong main
+        String excelFilePath = "D:\\Automation Testing\\AutoTest\\AutoTestD1/dataInfo.xlsx";
+        String sheetName = "Sheet1";
+
+        //doc du lieu tu file excel
+        List<Map<String, String>> excelData = ExcelUtils.readExcelData(excelFilePath, sheetName);
+
+        RequestDemoPage demoPage = new RequestDemoPage(driver);
+
+        //nhap row 1 khong có data Interest
+        Map<String, String> data = excelData.get(1);
+
+        demoPage.open();
+
+        demoPage.fillForm(
+                data.get("Business Email"),
+                data.get("First Name"),
+                data.get("Last Name"),
+                data.get("Company"),
+                data.get("Phone Number"),
+                data.get("Country"),
+                data.get("Interest"),
+                data.get("Comments")
+        );
+
+        WebElement submitButon = driver.findElement(By.xpath("//button[@type='submit']"));
+        submitButon.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement interestReqMsg = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='ValidMsgSolution_Interest__c']"))
+        );
+
+        //xac nhan hien thi require message
+        String actualMsg = interestReqMsg.getText();
+        String expetedMsg = "This field is required.";
+        Assert.assertEquals(actualMsg, expetedMsg);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("===== After Each Test Method =====");
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        System.out.println("===== Test Class Tear Down =====");
+    }
+
+    @AfterSuite
+    public void tearDownSuite() {
+        System.out.println("===== Test Suite Complete =====");
+    }
 }
